ccd43c17a5f4debb185ce57d10c9bf0d3e61973c
arvind0422 <arvind0422@gmail.com>
2017-06-02 17:30:29 +0530

    Improved Classification
    I have made a few changes in the algorithm. Made it more concrete and readable. Soon, I will upload the main program of which the classifier is a part.

--- Classifier.py
+++ Classifier.py
@@ -1,7 +1,7 @@
 """
 Classifier Program.
 
-Contains the function definition for classifying a given sequence
+Contains the functions for classifying a given sequence
 
 Once a model is trained, the model parameters are used to classify any given input. Feature Extraction and VQ
 is done before passing it here.
@@ -12,38 +12,48 @@ we go to Step 3.
 """
 
 import numpy as np
+from sklearn.cluster import KMeans
+from Model import Model  # Separate Class that models a HMM
 
+# PARAMETERS
 threshold = 0.000000  # Actual value to be decided.
 
 # all_models : list of models
 # observation : a list of numbers which correspond to column index of matrix model.b
 
 
-def classifier(all_models, observation, t, n):
+def quantize(model, feature_vector_list):
+    closest_cluster = np.zeros(len(feature_vector_list))
+    for feature_vector_index in np.arange(feature_vector_list):
+        closest_cluster[feature_vector_index] = model.codebook.predict(feature_vector_list[feature_vector_index])
+    return closest_cluster
+
+
+def forward(observation_list, model, n):
+    alpha = np.zeros((len(observation_list), n))
+    for i in np.arange(0, n, 1):
+        alpha[0][i] = model.pi[i] * model.b[i][0]
+    for t in np.arange(0, len(observation_list), 1):
+        for j in np.arange(0, n, 1):
+            for i in np.arange(0, n, 1):
+                alpha[t+1][j] = alpha[t+1][j] + alpha[t][i] * model.a[i][j] * model.b[j][t+1]
+    return np.sum(alpha[t-1, :])
+# Normalisation needs to be performed.
+
+
+def classifier(all_models, mfcc_features, n):
     probability = np.zeros(len(all_models))
+    t = len(mfcc_features)
     for model_index in np.arange(len(all_models)):
-        probability[model_index] = forward(all_models[model_index], observation, t, n)
+        observations = quantize(all_models[model_index], mfcc_features)
+        probability[model_index] = forward(all_models[model_index], observations, t, n)
     probability[probability < threshold] = 0.0
     # print(probability)
     if np.argmax(probability) > threshold:
         print(np.argmax(probability))
 
-    """
-    Optional Steps
-    Make a list of probabilities > threshold.
-    Choose based on difference.
-    """
-
-
-def forward(model, observation, t, n):
-    alpha = np.zeros(t, n)
-    alpha[0, :] = np.transpose(np.multiply(model.pi, model.b[:, observation[0]]))  # Initial Value : Return is a Row Vector
-    for tindex in np.arange(1, t, 1):
-        for j in np.arange(0, n, 1):
-            sum1 = 0
-            for i in np.arange(0, n, 1):
-                sum1 = sum1 + alpha[tindex][i]*model.a[i][j]
-            alpha[tindex, j] = np.multiply(model.b[j][observation[tindex]], sum1)
-    return np.sum(alpha[t-1, :])
-
-# Normalisation needs to be performedsss
+"""
+Optional Steps
+Make a list of probabilities > threshold.
+Choose based on difference.
+"""
