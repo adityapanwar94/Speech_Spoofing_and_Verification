57aad2cd20bcedc87ca8cbe936f365ce3ccb18df
arvind0422 <arvind0422@gmail.com>
2017-07-05 21:18:31 +0530

    Minor Changes

--- Speaker_Recognition/Training_GMM.py
+++ Speaker_Recognition/Training_GMM.py
@@ -16,48 +16,41 @@ from scipy.io import wavfile
 from hmmlearn.hmm import GMMHMM
 from python_speech_features import mfcc
 import pickle
-import os
+from os import listdir
 
 from Model import GMMModel
 
-# PARAMETERS.
+""" CREATING FILE TO STORE LEARNED MODEL """
+
+speaker_number = 10  # TODO: Add 1 before training a new speaker. Update this in the Classifier Program also.
+model_filename = "gmodel"+str(speaker_number)
+f = open(model_filename, "wb")
+
+""" PARAMTERS """
+
 N = 3  # Number of States.
 Mixtures = 128  # Number of Gaussian Mixtures.
 
-# CREATING A NEW FILE # If VCTK-Corpus is used.
-# speaker_number = 225
-# serial_number = 1  # TODO: Add 1 before training a new speaker. Update this in the Classifier Program also.
-# model_filename = "vmodel"+str(serial_number)
+""" CREATING LIST OF TRAINING FILE NAMES """
 
-# CREATING A NEW FILE. # If ELSDSR is used.
-speaker_number = 11  # TODO: Add 1 before training a new speaker. Update this in the Classifier Program also.
-model_filename = "gmodel"+str(speaker_number)
+# os.listdir(filepath) can also be used.  # TODO: Update file_names before training a new speaker.
+file_names = ["FAML_Sa.wav", "FAML_Sb.wav", "FAML_Sc.wav", "FAML_Sd.wav", "FAML_Se.wav", "FAML_Sf.wav", "FAML_Sg.wav"]
 
-f = open(model_filename, "wb")
+""" FEATURE EXTRACTION """
 
-# CREATING A LIST OF FILE NAMES # If VCTK-Corpus is used.
-# file_path = "/Users/arvindkumar/Desktop/Speaker Recognition/VCTK-Corpus/VCTK-Corpus/wav48/p"+str(speaker_number)
-# file_names = os.listdir(file_path)
-# training_test_split = 0.75  # TODO: Decide on the split.
-# number_of_files = int(0.training_test_split * len(file_names))
-# file_names = file_names[0:number_of_files]
-
-# CREATING A LIST OF FILE NAMES # If ELSDSR is used.
-file_names = ["FMEL_Sa.wav", "FMEL_Sb.wav", "FMEL_Sc.wav", "FMEL_Sd.wav", "FMEL_Se.wav", "FMEL_Sf.wav", "FMEL_Sg.wav"]
-
-# READING INPUT & MFCC FEATURE EXTRACTION.
 lengths = np.empty(len(file_names))
 feature_vectors = np.empty([0, 13])
 for i in range(len(file_names)):
-    # rate, x = wavfile.read(file_path+str(speaker_number)+"/"+file_names[i])  # If VCTK-Corpus is used.
     rate, x = wavfile.read(file_names[i])
     x = mfcc(x, samplerate=rate)
     lengths[i] = len(x)
     feature_vectors = np.concatenate((feature_vectors, x))
-# print(np.mean(feature_vectors))
-# print(lengths)
+# print(feature_vectors.shape)
 
-# INITIAL PARAMETERS FOR HMM.
+""" MODEL WITH INITIAL PARAMETERS """
+
+# Might provide better results in some cases.
+
 # startprob = np.ones(N) * (10**(-30))  # Left to Right Model
 # startprob[0] = 1.0 - (N-1)*(10**(-30))
 # transmat = np.zeros([N, N])  # Initial Transmat for Left to Right Model
@@ -66,19 +59,24 @@ for i in range(len(file_names)):
 #         transmat[i, j] = 1/(N-i)
 # transmat = np.triu(transmat, k=0)
 # transmat[transmat == 0] = (10**(-30))
+# model = GMMHMM(n_components=N, n_mix=Mixtures, covariance_type='diag', init_params="mcw")
+# model.startprob_ = startprob
+# model.transmat_ = transmat
 
-# GMM-HMM MODEL FITTING.
+""" MODEL WITHOUT INITIAL PARAMETERS """
+
 model = GMMHMM(n_components=N, n_mix=Mixtures, covariance_type='diag')
+
+""" MODEL FITTING """
+
 model.fit(feature_vectors)
 
-# STORING THE MODEL. # If VCTK-Corpus is used.
-# sample = GMMModel(model, "p"+str(speaker_number))
+""" STORING THE MODEL """
 
-# STORING THE MODEL. If ELSDSR is used.
-sample = GMMModel(model, "FMEL")
-
+sample = GMMModel(model, "FAML")  # TODO: Change Name as well.
 pickle.dump(sample, f)
 
-# POSSIBLE EXTENSIONS.
-# TODO: Plug in various Initialisation Values and use score method to find the best fit model.
-# TODO: Try to improve the way in which multiple models are trained.
+"""" FUTURE EXTENSIONS """
+
+# TODO: Use score method to evaluate the model and run multiple iterations until best fit.
+# TODO: Create a loop so that multiple speakers can be trained in one run.
